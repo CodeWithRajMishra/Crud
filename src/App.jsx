51e19dd32295cc76b53a79c9f49cae2a0e32786a
CodeWithRajMishra <rajmishra3@gmail.com>
2025-02-18 16:10:05 +0530

    pro

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Registration from "./pages/Registration";
-import DashBoard from "./pages/DashBoard";
+import axios from "axios";
+
 const App=()=>{
-    return(
+const hendleSubmit=async()=>{
+  let api="http://localhost:8000/myhome";
+  const res= await axios.get(api);
+  console.log(res);
+}
+
+const hendleSubmit1=async()=>{
+  let api="http://localhost:8000/about";
+  const res= await axios.get(api);
+  console.log(res);
+}
+
+const hendleSubmit2=async()=>{
+  let api="http://localhost:8000/service";
+  const res= await axios.get(api);
+  console.log(res);
+}
+
+  return(
         <>
-          <BrowserRouter>
-             <Routes>
-                <Route path="/" element={<Layout/>}>
-                 <Route index element={<Home/>} />
-                 <Route path="home" element={<Home/>} />
-                 <Route path="login" element={<Login/>} />
-                 <Route path="registration" element={<Registration/>}/>
-                 <Route path="dashboard" element={<DashBoard/>}/>
-                </Route>
-             </Routes>
-          </BrowserRouter>
+         <h1> Welcome To My App</h1>
+
+         <button onClick={hendleSubmit}> Home here!</button>
+         <button onClick={hendleSubmit1}> About btn!</button>
+         <button onClick={hendleSubmit2}> Service btn!</button>
         </>
+
     )
 }
 
